76587742f4475f5f6c1b79e5c224e5e08a247a44
retini <retini@open2b.com>
2023-03-05 11:15:43 +0100

    Move `ProductGallery` in the components directory

---
+++ app/components/ProductGallery.jsx
@@ -0,0 +1,49 @@
+import {MediaFile} from '@shopify/hydrogen-react';
+
+function ProductGallery({media}) {
+  if (!media.length) {
+    return null;
+  }
+
+  const typeNameMap = {
+    MODEL_3D: 'Model3d',
+    VIDEO: 'Video',
+    IMAGE: 'MediaImage',
+    EXTERNAL_VIDEO: 'ExternalVideo',
+  };
+
+  return (
+    <div>
+      {media.map((med, i) => {
+        let extraProps = {};
+
+        if (med.mediaContentType === 'MODEL_3D') {
+          extraProps = {
+            interactionPromptThreshold: '0',
+            ar: true,
+            loading: 'eager',
+            disableZoom: true,
+            style: {height: '100%', margin: '0 auto'},
+          };
+        }
+
+        const data = {
+          ...med,
+          __typename: typeNameMap[med.mediaContentType] || typeNameMap['IMAGE'],
+          image: {
+            ...med.image,
+            altText: med.alt || 'Product image',
+          },
+        };
+
+        return (
+          <div key={data.id || data.image.id}>
+            <MediaFile tabIndex="0" data={data} {...extraProps} />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default ProductGallery;
--- app/routes/$productID.jsx
+++ app/routes/$productID.jsx
@@ -1,6 +1,6 @@
 import {json} from 'react-router';
 import {useLoaderData} from '@remix-run/react';
-import {MediaFile} from '@shopify/hydrogen-react';
+import ProductGallery from '../components/ProductGallery';
 
 async function loader({request, context, params}) {
   let {productID} = params;
@@ -27,90 +27,6 @@ function Product() {
     </div>
   );
 }
-
-function ProductGallery({media}) {
-  if (!media.length) {
-    return null;
-  }
-
-  const typeNameMap = {
-    MODEL_3D: 'Model3d',
-    VIDEO: 'Video',
-    IMAGE: 'MediaImage',
-    EXTERNAL_VIDEO: 'ExternalVideo',
-  };
-
-  return (
-    <div>
-      {media.map((med, i) => {
-        let extraProps = {};
-
-        if (med.mediaContentType === 'MODEL_3D') {
-          extraProps = {
-            interactionPromptThreshold: '0',
-            ar: true,
-            loading: 'eager',
-            disableZoom: true,
-            style: {height: '100%', margin: '0 auto'},
-          };
-        }
-
-        const data = {
-          ...med,
-          __typename: typeNameMap[med.mediaContentType] || typeNameMap['IMAGE'],
-          image: {
-            ...med.image,
-            altText: med.alt || 'Product image',
-          },
-        };
-
-        return (
-          <div key={data.id || data.image.id}>
-            <MediaFile tabIndex="0" data={data} {...extraProps} />
-          </div>
-        );
-      })}
-    </div>
-  );
-}
-
-const PRODUCT_QUERY = `#graphql
-  query product($productID: ID) {
-    product(id: $productID) {
-      id
-      title
-      handle
-      vendor
-      descriptionHtml
-      media(first: 10) {
-        nodes {
-          ... on MediaImage {
-            mediaContentType
-            image {
-              id
-              url
-              altText
-              width
-              height
-            }
-          }
-          ... on Model3d {
-            id
-            mediaContentType
-            sources {
-              mimeType
-              url
-            }
-          }
-        }
-      }
-      options {
-        name,
-        values
-      }
-    }
-  }
-`;
 
 export default Product;
 export {loader};
