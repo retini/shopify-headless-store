d97bf8a2e0e34405fd464af5e339374de2552173
retini <retini@open2b.com>
2023-03-06 22:34:31 +0100

    Add link to logo

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -1,9 +1,12 @@
+import {Link} from '@remix-run/react';
+
 function Layout({children}) {
   return (
     <div className="layout">
       <div className="logo">
         <span>Headless</span>
         <img src="office-chair.png" alt="" />
+        <Link to={`/`}></Link>
       </div>
       <div className="content">{children}</div>
     </div>
